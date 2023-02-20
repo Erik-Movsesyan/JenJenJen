bee62a93482dd0d52dcbde7e9ce7e38bd0ef83ad
ErikGit <ericmovsessian71@gmail.com>
2023-02-20 10:47:23 +0400

    First Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace env {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
